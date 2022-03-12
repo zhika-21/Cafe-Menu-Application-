3d87ad143b012f0d52813f1adbf27b95c5cbc0aa
Jibek <jibek260886@gmail.com>
2022-03-12 13:58:45 +0300

    add button component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,17 +10,19 @@ const Menu = ({items}) => {
       {items.map((menuItem) => {
         const {id, title, img, desc, price} = menuItem;
         return (
-          <article key={id} className="menu-item">
-            <img src={img} alt={title} className="photo" />
-            <div className="item-info">
-              <header>
-                <h4>{title}</h4>
-                <h4 className="price">₹{Math.floor(price) * 10}</h4>
-              </header>
-              <p className="item-text">{desc}</p>
-            </div>
-          </article>
-          <Button el={menuItem}/>
+          <>
+            <article key={id} className="menu-item">
+              <img src={img} alt={title} className="photo" />
+              <div className="item-info">
+                <header>
+                  <h4>{title}</h4>
+                  <h4 className="price">₹{Math.floor(price) * 10}</h4>
+                </header>
+                <p className="item-text">{desc}</p>
+              </div>
+            </article>
+            <Button el={menuItem} />
+          </>
         );
       })}
     </div>
