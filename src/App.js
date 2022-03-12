f7cc57b403d16432b0fe207a105c420a3463fd85
Millana <chotubaevam@gmail.com>
2022-03-12 16:31:46 +0600

    added API beside of local data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import React, { useState } from "react";
-import Menu from "./components/Menu";
+import React, { useEffect, useState } from "react";
 import Categories from "./Categories";
-import items from "./assets/data";
-const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+import axios from "axios";
+
 
 function App() {
-  const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState(allCategories);
+  const [menuItems, setMenuItems] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  const [filteredFoodList, setFilteredFoodList] = useState([])
+  const [categories, setCategories] = useState([])
 
   //Millana
   const [data, setData] = useState([])
   //const [isLoading, setIsLoading] = useState(true)
 
   //Want to make API
-  // const getData = async() => {
-  //   const api = await axios.get("https://mocki.io/v1/491c6463-28ea-404f-bfe4-86fecd77fa67")
-  //   setData(api)
-  // }
-  const filterItems = (category) => {
-    if (category === "all") {
-      setMenuItems(items);
-      return;
-    }
-    const newItems = items.filter((item) => item.category === category);
-    setMenuItems(newItems);
-  };
+  const getData = async() => {
+    const api = await axios.get("https://mocki.io/v1/491c6463-28ea-404f-bfe4-86fecd77fa67")
+    setData(api.data.menu)
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+
+  useEffect(() => {
+      let menuItems = data.filter((item) => item.category === selectedCategory || selectedCategory === "all");
+      setFilteredFoodList(menuItems);
+  }, [])
+
+  const categoryList = ["all", ...new Set(menuItems.map((item) => item.category))];
 
   return (
     <main>
@@ -33,8 +36,8 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories categories={categories} filterItems={filterItems} />
-        <Menu items={menuItems} />
+        <Categories categories={categories} filteredFoodList={filteredFoodList} />
+        {/* <Menu data={data} /> */}
       </section>
     </main>
   );
