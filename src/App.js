ca6af1bafcb4f29b85273ecc53136666d9f0d961
Ainura <abekbolsunova@gmail.com>
2022-03-16 17:12:03 +0600

    Cheking commit

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
       <section className="menu section">
         <div className="title">
           <h2>our menu</h2>
+          <h3>TESTING MY FIRST COMMIT</h3>
           <div className="underline"></div>
         </div>
         <Categories categories={categories} filterItems={filterItems} />
